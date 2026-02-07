3878845e3089802f60a843cdad4a2cb347d0bf34
Benlite777 <benhurskumar@gmail.com>
2026-02-07 14:39:03 +0530

    init database

--- tests/init_database.sql
+++ tests/init_database.sql
@@ -1,11 +1,24 @@
 USE master;
+GO
 
+
+-- Drop and recreate the 'Datawarehouse' database
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name='DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+
+GO
+
+-- Create the Datawarehouse database
 CREATE DATABASE DataWarehouse;
 
 USE DataWarehouse;
-
-
 GO
+
+-- Create Schemas
 CREATE SCHEMA bronze;
 GO
 CREATE SCHEMA silver;
