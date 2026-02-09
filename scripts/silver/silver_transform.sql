1a5124ea6046483b2baf6b872f7d67c8e7a966a5
Benlite777 <benhurskumar@gmail.com>
2026-02-09 21:50:23 +0530

    Gold Layer Completed

--- scripts/silver/silver_transform.sql
+++ scripts/silver/silver_transform.sql
@@ -182,16 +182,16 @@ BEGIN
             CASE
                 WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
                 ELSE cid
-            END,
+            END as cid,
             CASE
                 WHEN bdate > GETDATE() THEN NULL
                 ELSE bdate
-            END,
+            END as bdate,
             CASE
                 WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
                 WHEN UPPER(TRIM(gen)) IN ('M', 'MALE')   THEN 'Male'
-                ELSE 'n/a'
-            END
+                ELSE gen
+            END as gen
         FROM bronze.erp_cust_az12;
 
         /* ===============================
